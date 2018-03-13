6693290338fca4bfefacbd9facbcdaaaba5d686e
RicardoM7u <qq1683715827@gmail.com>
2018-03-13 18:05:01 +0800

    0 0b 0x int float

--- Ivor Horton/Hello_2_7.c
+++ Ivor Horton/Hello_2_7.c
@@ -1,1 +1,27 @@
 //Program 2.7 Division with float vaules
+#include <stdio.h>
+
+int main (void)
+{
+	char a;
+	short int b;
+	int c;
+	long int d;
+	long long int e;
+	
+	unsigned char h;
+	unsigned short i;
+	unsigned int j;
+	unsigned long k;
+	unsigned long long l;
+	
+	int o = -1;
+	long p = 2L;
+	long long q = 3LL;
+	unsigned int r = 4U;
+	unsigned long s =5UL;
+	unsigned long long t = 6ULL;
+	
+	float u = 7f;
+	double v = 8.9E14;
+	long double w = 1.1E17;
